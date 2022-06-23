f4e8e00c56dcea650b8802d8c324a6277dd5e5f6
Admin <brycealvir@gmail.com>
2022-06-23 12:13:26 -0700

    Added schema

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,15 +5,34 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <div>
-                <h3>Rating</h3>
-                    <p>Currently Unrated</p>
+          <div className="row">
+            <div className="col-sm-6">
+              <img src={data.place.pic} alt={data.place.name} />
+              <h3>
+                Located in {data.place.city}, {data.place.state}
+              </h3>
             </div>
-            <div>
-                <h3>Comments</h3>
-                    <p>No comments yet!</p>
+            <div className="col-sm-6">
+              <h1>{ data.place.name }</h1>
+                  <h2>
+                  Description
+                  </h2>
+                  <h3>
+                  {data.place.showEstablished()}
+                  </h3>
+                  <h4>
+                  Serving {data.place.cuisines}
+                  </h4>
             </div>
+            <div className="col-sm-6">
+              <h3>Rating</h3>
+                <p>Currently Unrated</p>
+            </div>
+            <div className="col-sm-6">
+              <h3>Comments</h3>
+                <p>No comments yet!</p>
+            </div>
+          </div>
               <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                 Edit
               </a>     
