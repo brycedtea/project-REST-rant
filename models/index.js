cc0ac7399d4b143dd55de1e3dc6db61d3fcabbba
Admin <brycealvir@gmail.com>
2022-06-23 19:36:32 -0700

    implemented comment form

--- models/index.js
+++ models/index.js
@@ -6,5 +6,6 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 })
 
-module.exports.Place = require('./places')
 
+module.exports.Place = require('./places')
+module.exports.Comment = require('./comment')
